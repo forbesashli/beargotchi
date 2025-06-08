c9ecb2e31cfcada91e07178e8ba348420e1bbfb7
Ashli <forbesashli@gmail.com>
2025-06-08 12:03:16 -0700

    some other stuff

---
+++ h7/nucleo-h7-setup/Core/Src/seven_seg.c
@@ -0,0 +1,10 @@
+/*
+ * seven_seg.c
+ *
+ *  Created on: Jun 7, 2025
+ *      Author: Ashli
+ *
+ *  File for 7 segment display
+ */
+
+
---
+++ h7/nucleo-h7-setup/Debug/Core/Src/seven_seg.d
@@ -0,0 +1,1 @@
+Core/Src/seven_seg.o: ../Core/Src/seven_seg.c
